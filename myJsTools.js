98966d51cec9a131e37c876d996d579293b78363
linghaoxinpian <530060499@qq.com>
2019-08-07 14:15:40 +0800

    Update myJsTools.js

--- myJsTools.js
+++ myJsTools.js
@@ -46,3 +46,77 @@ function convertSpecialCharacter(strOrArr) {
     }
     return strOrArr;
 }
+
+/**
+ * matching common prefix for string array
+ * @param {Array} str
+ * @returns "" or prefix
+ */
+function commonPrefix(strs) {
+    if (strs.length == 0) return "";
+    let prefix = strs[0];
+    for (let i = 1; i < strs.length; i++)
+        while (strs[i].indexOf(prefix) != 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+            if (prefix == "") return "";
+        }
+    return prefix;
+}
+
+/**
+ * will be removes the specified prefix symbol
+ * @param {Array} data
+ * @param {string} prefix - default string is "."
+ */
+namespace.removePrefixForData = function (data, prefix) {
+    if (!prefix) {
+        prefix = ".";
+    }
+    let results = [];
+    for (let i = 0; i < data.length; i++) {
+        let record = {};
+        for (let key in data[i]) {
+            let propName = key;
+            if (key && key.indexOf(prefix) >= 0) {
+                propName = key.substring(key.indexOf(prefix) + 1);
+            }
+            record[propName] = data[i][key];
+        }
+        results.push(record);
+    }
+    return results;
+}
+
+/**
+ * xss过滤
+ */
+function escapeHtml(value) {
+    if (typeof value !== 'string') {
+      return value
+    }
+    return value.replace(/[&<>`"'\/]/g, function(result) {
+      return {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '`': '&#x60;',
+        '"': '&quot;',
+        "'": '&#x27;',
+        '/': '&#x2f;',
+      }[result]
+    })
+  }
+
+/**
+ * topElement 为jQuery对象
+ */
+function hasEmptyForRequiredFields(topElement){
+    let fields = topElement.find("[required]");
+    for(let field of fields){
+        let val = $(field).val();
+        if(!val){                
+            return true;
+        }
+    }
+    return false;
+}
